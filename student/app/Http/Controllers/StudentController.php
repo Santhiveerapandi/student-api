72f2fd1bac679e0dc8d42027198b984e0c3ef87a
santhiveerapandi <santhiveerapandi@gmail.com>
2023-12-11 20:06:18 +0530

    first commit

---
+++ student/app/Http/Controllers/StudentController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    protected $student;
+    public function __construct(){
+        $this->student = new Student();
+        
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return $this->student->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return $this->student->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+       return $student = $this->student->find($id); 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $student = $this->student->find($id);
+        $student->update($request->all());
+        return $student;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $student = $this->student->find($id);
+        return $student->delete();
+    }
+}
