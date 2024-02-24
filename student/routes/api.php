48cdb38e3a3f4514668e5f0082804836e2961a62
santhiveerapandi <santhiveerapandi@gmail.com>
2024-02-24 11:44:06 +0530

    Task List Api Creation

--- student/routes/api.php
+++ student/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TaskListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('/student', StudentController::class);
+Route::apiResource('/tasklist', TaskListController::class);
